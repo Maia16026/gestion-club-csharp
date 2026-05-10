ae6cc42d971feecfe943554811569f85f271f94e
Maia Guadalupe <maia16026corral@gmail.com>
2026-05-10 15:23:56 -0300

    Funciones Club.cs
    Se agregan funciones como eliminar deporte, listar inscriptos y listar deudores para cumplir con el encapsulamiento.

--- proyecto/Club.cs
+++ proyecto/Club.cs
@@ -6,20 +6,92 @@ namespace proyecto
 	public class Club
 	{
 		private string nombre;
-		private ArrayList deportes;
-		private ArrayList entrenadores;
+		private List<Deporte> deportes;
+		private List<Entrenador> entrenadores;
 		
 		public Club(string nombre)
 		{
 			this.nombre=nombre;
-			deportes=new ArrayList();
-			entrenadores=new ArrayList();
+			deportes=new List<Deporte>();
+			entrenadores=new List<Entrenador>();
 		}
-		public ArrayList Deportes{
-			get{ return deportes; }
+		public void AltaEntrenador(Entrenador entrenador)
+        {
+            entrenadores.Add(entrenador);
 		}
-		public ArrayList Entrenadores{
-			get{ return entrenadores; }
+		public void BajaEntrenador(int dni)
+        {
+            Entrenador entrenador = BuscarEntrenador(dni);
+
+            entrenadores.Remove(entrenador);
+		}
+		public Entrenador BuscarEntrenador(int dni)
+        {
+            foreach (Entrenador e in entrenadores)
+            {
+                if (e.Dni == dni)
+                {
+                    return e;
+                }
+            }
+
+            throw new Exception("Entrenador no encontrado");
+		}
+		public void AgregarDeporte(Deporte deporte)
+        {
+            deportes.Add(deporte);
+		}
+		public void EliminarDeporte(string nombre, string categoria)
+        {
+            Deporte deporte = BuscarDeporte(nombre, categoria);
+
+            if (deporte.Inscriptos.Count > 0)
+            {
+                throw new Exception("El deporte tiene inscriptos");
+            }
+
+            deportes.Remove(deporte);
+		}
+		public Deporte BuscarDeporte(string nombre, string categoria)
+        {
+            foreach (Deporte d in deportes)
+            {
+                if (
+                    d.Nombre.ToLower() == nombre.ToLower()
+                    &&
+                    d.Categoria.ToLower() == categoria.ToLower()
+                )
+                {
+                    return d;
+                }
+            }
+
+            throw new Exception("Deporte no encontrado");
+		}
+		public void ListarInscriptos()
+        {
+            foreach (Deporte d in deportes)
+            {
+                Console.WriteLine("\nDEPORTE: "+ d.Nombre + " - "+ d.Categoria);
+
+                foreach (Nino n in d.Inscriptos)
+                {
+                    Console.WriteLine(n.Nombre + " | DNI: "+ n.Dni);
+                }
+            }
+		}
+		public void ListarDeudores(int mesActual)
+        {
+            foreach (Deporte d in deportes)
+            {
+                foreach (Nino n in d.Inscriptos)
+                {
+                    if (n.MesPago < mesActual)
+                    {
+                        Console.WriteLine( n.Nombre+ " | DNI: "+ n.Dni+ " | Deuda desde mes "+ n.MesPago);
+                    }
+                }
+            }
 		}
 	}
 }
