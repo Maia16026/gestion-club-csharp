ab16a7a9ab0b13381e52dd9dcea322338d396637
Maia Guadalupe <maia16026corral@gmail.com>
2026-05-10 15:11:21 -0300

    Funciones Deporte.cs
    Se realiza una encapsulación agregando las funciones como inscripción (antes en el program) correspondientes a la clase de deporte.

--- proyecto/Deporte.cs
+++ proyecto/Deporte.cs
@@ -11,7 +11,7 @@ namespace proyecto
 		private string categoria;
 		private double costoCuota;
 		private Entrenador entrenador;
-		private ArrayList inscriptos;
+		private List<Nino> inscriptos;
 			
 		public Deporte(string nombre, string categoria, int cupo, Entrenador entrenador, double costoCuota)
 		{
@@ -20,7 +20,7 @@ namespace proyecto
 			this.categoria=categoria;
 			this.entrenador=entrenador;
 			this.costoCuota=costoCuota;
-			inscriptos=new ArrayList();
+			inscriptos=new List<Nino>();
 		}
 		public string Nombre{
 			set{nombre=value;}
@@ -38,8 +38,39 @@ namespace proyecto
 			set{costoCuota=value;}
 			get{return costoCuota;}
 		}
-		public ArrayList Inscriptos{
+		public List<Nino> Inscriptos{
 			get{return inscriptos;}
+		}
+		public void AgregarInscripto(Nino nino)
+        {
+            if (cupo <= 0)
+            {
+                throw new Exception("No hay cupo");
+            }
+
+            inscriptos.Add(nino);
+
+            cupo--;
+		}
+		public void EliminarInscripto(int dni)
+        {
+            Nino nino = BuscarNino(dni);
+
+            inscriptos.Remove(nino);
+
+            cupo++;
+		}
+		public Nino BuscarNio(int dni)
+        {
+            foreach (Nino n in inscriptos)
+            {
+                if (n.Dni == dni)
+                {
+                    return n;
+                }
+            }
+
+            throw new Exception("Niño no encontrado");
 		}
 	}
 }
